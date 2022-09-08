a8b241e21cef49b3557ce534b8cf61bfeed9307a
leeduckgo <albertschr@163.com>
2022-09-08 12:21:34 +0800

    update NFT gallery

--- lib/super_issuer_web/router.ex
+++ lib/super_issuer_web/router.ex
@@ -80,22 +80,4 @@ defmodule SuperIssuerWeb.Router do
     get "/nft/test", AppController, :test
   end
 
-  # ports that allow cross domain
-
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      # live_dashboard "/dashboard", metrics: SuperIssuerWeb.Telemetry
-    end
-  end
 end
